e378cf18853da42b703da429e70bbf3a6259eaff
arunkumar gm <arungmrnr@gmail.com>
2025-07-17 10:33:35 +0530

    fix: build fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,9 +21,9 @@ export default function RootLayout({
         
     <html lang="en">
       <body>      
-        <Authenticator>
+        {/* <Authenticator> */}
           {children}
-        </Authenticator>
+        {/* </Authenticator> */}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-"use client";
-
-import { useState, useEffect } from "react";
+'use client'
+import { useState, useEffect, Children } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
@@ -8,6 +7,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
@@ -17,7 +17,7 @@ interface ListTodosData {
     items: Array<Schema["Todo"]["type"]>;
   }
 
-export default function App() {
+function App() {
     
   const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
@@ -41,23 +41,36 @@ export default function App() {
     });
   }
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}<button onClick={()=>{deleteTodo(todo.id)}}>X</button></li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+  return (    
+      <main>
+        <h1>My todos</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.content}<button onClick={()=>{deleteTodo(todo.id)}}>X</button></li>
+          ))}
+        </ul>
+        <div>
+          🥳 App successfully hosted. Try creating a new todo.
+          <br />
+          <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+            Review next steps of this tutorial.
+          </a>
+        </div>
+        <button onClick={signOut}>Sign out</button>
+      </main>
   );
 }
+
+// Corrected withAuth HOC
+function withAuth(AppComponent: React.FC) {
+  return function AuthenticatedApp(props: any) { // This now returns a function
+    return (
+      <Authenticator>
+        <AppComponent {...props} />
+      </Authenticator>
+    );
+  };
+}
+
+export default withAuth(App);
